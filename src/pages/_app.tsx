5f2e086cd9174e50f2fc20c820e8a25172912e62
Manuel Lara <smazzbass@gmail.com>
2023-06-23 20:08:06 -0600

    Moved to  MySQL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+  
+    
+      <Component {...pageProps} />
+       
+  ) 
+  
 };
 
 export default api.withTRPC(MyApp);
