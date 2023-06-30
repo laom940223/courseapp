f64e705906d6aa07e1d6900072de60e171422bbe
Manuel Lara <smazzbass@gmail.com>
2023-06-30 15:39:19 -0600

    Main Layout with auth flow

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,30 +1,81 @@
 
-import { AppShell, Navbar, Header, Button, Title, Flex, Grid, Modal } from '@mantine/core';
+import { AppShell, Header, Button, Title, Flex, Grid, Menu, Group, Avatar, rem, UnstyledButton, Loader } from '@mantine/core';
 
-import React from 'react'
-import { useUser, SignInButton, SignOutButton } from "@clerk/nextjs";
+import React, {forwardRef} from 'react'
+import { useUser, SignInButton, SignOutButton, useClerk, SignUpButton } from "@clerk/nextjs";
+import { IconExternalLink } from '@tabler/icons-react';
+import Link from 'next/link';
+
+
+
+
+
+interface UserButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  image: string;
+  name: string;
+  
+}
+
+
+
+/* eslint-disable react/display-name */
+const AvatarTarget  = forwardRef<HTMLButtonElement, UserButtonProps>(
+  ({ image, name, ...others }: UserButtonProps, ref) => (
+    <UnstyledButton
+      ref={ref}
+      sx={(theme) => ({
+        display: 'block',
+        
+        
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+        '&:hover': {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      })}
+      {...others}
+    >
+      <Group>
+        <Avatar src={image} radius="xl" />
+      </Group>
+    </UnstyledButton>
+  )
+);
+
+
+
+
 
 export default function Layout ({children} : {children: React.ReactNode}){
 
 
-    const {user} = useUser()
+    const {user, isLoaded} = useUser()
+    const { signOut} = useClerk()
+
+    const onClickLogOut =  ()=>{
+
+
+      signOut().then(()=>{
+        console.log("Loggin oout")
+      }).catch(err=>{console.log("Something went wrong")})
+    }
+
+
+
+    if(!isLoaded) return    <Loader />;
 
 
     return (
       
     <AppShell
       padding="md"
-    //   navbar={
-    //     <Navbar width={{ base: 300 }} height={500} p="xs">
-
-           
-    //     </Navbar>}
-      
+  
       
       header={<Header height={60} p="md">
         
-               
-            <Grid columns={24} >
+                
+            <Grid columns={24} sx={{padding:"0 4em"}} >
                 <Grid.Col span={12}>
                     <Flex  dir="row"  justify={"flex-start"} align={"center"}>
                         <Title order={3}>App</Title>
@@ -36,22 +87,50 @@ export default function Layout ({children} : {children: React.ReactNode}){
                     <Flex  dir="row"  justify={"flex-end"} gap={"lg"} align={"center"} style={{paddingRight:"40px"}} >
 
                         {!user ?     
-                        
-                            <SignInButton mode="modal">
-                                <Button  color="cyan" size="md">
-                                     Sign In
-                                </Button>
-                            </SignInButton>
-                            :
+                            <>
+                              <SignInButton mode="modal">
+                                  <Button  color="cyan" size="md">
+                                        Sign In
+                                  </Button>
+                              </SignInButton>
+                              <SignUpButton mode='modal'>
+                                  <Button  color="cyan" size="md">
+                                        Sign Up
+                                  </Button>
+                              </SignUpButton>
+                            </>
+                            :<>
                             
-                            <SignOutButton >
-                            <Button  color="cyan" size="md">
-                                 Sign Out
-                            </Button>
-                        </SignOutButton>
-                    
-                    }
-                       
+                            <Menu withArrow>
+                              <Menu.Target>
+                                
+                                
+                                  <AvatarTarget image={user.imageUrl} name={user.username!} />
+                                  
+                              
+
+                            
+                            
+
+                              </Menu.Target>
+                                    <Menu.Dropdown sx={{padding:".5em 1em"}}>
+                                    <Menu.Label>Hola {`${user.firstName!}`}</Menu.Label>
+                                        <Menu.Item
+                                          icon={<IconExternalLink size={rem(14)} />}
+                                          onClick={onClickLogOut}
+                                          
+                                        >
+                                          Log Out
+                                        </Menu.Item>
+                                  </Menu.Dropdown>
+
+                            </Menu>
+
+                            
+                            
+                            </>                  
+                      }
+                        
                             
                         
                     </Flex>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
+import { useUser } from "@clerk/nextjs";
 import styles from "./index.module.css";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
-import { useUser,SignOutButton } from "@clerk/nextjs";
+
 
 export default function Home() {
+
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
- 
+ const { user }  = useUser()
 
+ 
+ 
   return (
     <>
       <Head>
